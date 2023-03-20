2e6d0418628ac81a7b91d79a7ad6bb894259c175
GUANTAI001 <jpguantai@gmail.com>
2023-03-20 17:50:00 +0300

    update

--- portifolio/src/components/About.jsx
+++ portifolio/src/components/About.jsx
@@ -52,6 +52,7 @@ function About(){
                                         <li> -> CSS3</li>
                                         <li> -> JavaScript</li>
                                         <li> -> React Js</li>
+                                        <li> -> Ruby on Rails</li>
                                        
                                    </ul>
 
@@ -94,7 +95,7 @@ function About(){
                                         
                                           <h2 class="section-title">About Me</h2>
                                            <p class="aboutme">
-                                           As a developer in coding websites and mobile applications, I am enthusiastic and passionate about learning and creating things with code. Although I am still early in my journey, I have gained some basic knowledge and experience and am eager to continue expanding my skills, particularly in the areas of HTML, CSS, JavaScript, and Kotlin for Android development.
+                                           As a developer in coding websites and mobile applications, I am enthusiastic and passionate about learning and creating things with code. Although I am still early in my journey, I have gained some basic knowledge and experience and am eager to continue expanding my skills, particularly in the areas of HTML, CSS, JavaScript, React JS , Ruby on Rails and Kotlin for Android development.
 
                                              </p>
             
